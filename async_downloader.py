7a386e260d2ca94011c63bec27036ae08094c111
lina5274 <167440004+lina5274@users.noreply.github.com>
2024-07-17 15:20:28 +0300

    Update async_downloader.py

--- async_downloader.py
+++ async_downloader.py
@@ -3,19 +3,23 @@ import asyncio
 import hashlib
 import os
 import tempfile
-import re
+from bs4 import BeautifulSoup
 
-async def get_repo_files(session, repo_url):
-    async with session.head(repo_url) as resp:
-        if resp.status != 200:
-            raise Exception("Failed to fetch repository files")
-        content_disposition = resp.headers.get('Content-Disposition')
-        if content_disposition:
-            match = re.search(r'filename="(.*?)"', content_disposition)
-            if match:
-                return match.group(1)
-        else:
-            raise Exception("No Content-Disposition header found")
+
+async def get_all_file_urls(session, repo_url):
+    async with session.get(repo_url) as resp:
+        html_content = await resp.text()
+
+    soup = BeautifulSoup(html_content, 'lxml')
+    links = soup.find_all('a', href=True)
+    file_links = []
+    for link in links:
+        href = link['href']
+        # Фильтрация только txt файлов
+        if href.endswith('.txt'):
+            file_links.append(href)
+    return file_links
+
 
 async def download_file(session, url):
     filename = os.path.join(tempfile.gettempdir(), url.split('/')[-1])
@@ -28,22 +32,23 @@ async def download_file(session, url):
                 f_handle.write(chunk)
     return filename
 
-async def main(repo_url):
-    async with aiohttp.ClientSession() as session:
-        try:
-            file_url = await get_repo_files(session, repo_url)
-            file_path = await download_file(session, file_url)
-            sha256_hash = calculate_sha256(file_path)
-            return sha256_hash
-        except Exception as e:
-            print(f"Error processing {repo_url}: {e}")
-
-def calculate_sha256(file_path):
+async def calculate_sha256_for_file(file_path):
     sha256_hash = hashlib.sha256()
     with open(file_path, "rb") as f:
         for byte_block in iter(lambda: f.read(4096), b""):
             sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
+
+async def main(repo_url):
+    async with aiohttp.ClientSession() as session:
+        try:
+            file_urls = await get_all_file_urls(session, repo_url)
+            download_tasks = [download_file(session, url) for url in file_urls]
+            downloaded_files = await asyncio.gather(*download_tasks)
+            sha256_hashes = [calculate_sha256_for_file(file) for file in downloaded_files]
+            return sha256_hashes
+        except Exception as e:
+            print(f"Error processing {repo_url}: {e}")
 
 if __name__ == "__main__":
     repo_url = "https://gitea.radium.group/radium/project-configuration"
